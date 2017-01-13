7465da9eaa70fd123bc8fbe7af9efe9ce95eb7eb
sijian.gu <sijian.gu@genewiz.com>
2017-01-13 17:23:16 +0800

    new vue component

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,69 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li>
+        <a href="https://vuejs.org" target="_blank">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
+      </li>
+      <li>
+        <a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+      </li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+      </li>
     </ul>
+    <button @click="showModal=true">show</button>
+    <div>{{showModal}}</div>
+    <modal v-if="showModal" @close="showModal=false"></modal>
+
+    <elasticHeader>
+      <template slot="header">
+        <h1>Elastic Draggable SVG Header</h1>
+        <p>
+          with
+          <a href="http://vuejs.org">Vue.js</a>
+          +
+          <a href="http://dynamicsjs.com">dynamics.js</a>
+        </p>
+      </template>
+      <template slot="content">
+        <p>
+          Note this is just an effect demo - there are of course many additional details if you want to use this in production, e.g. handling responsive sizes, reload threshold and content scrolling. Those are out of scope for this quick little hack. However, the idea is that you can hide them as internal details of a Vue.js component and expose a simple Web-Component-like interface.
+        </p>
+      </template>
+    </elasticHeader>
   </div>
 </template>
 
 <script>
+import modal from './component/modal.vue'
+import elasticHeader from  './component/elastic-header.vue'
 export default {
   name: 'app',
   data :function() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      showModal:false
     }
-  }
+  },
+  components:{modal,elasticHeader}
 }
 </script>
 
