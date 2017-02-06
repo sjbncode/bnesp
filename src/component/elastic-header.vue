32fe4180c1894ca574a4143573d59c3419927ce7
sijian.gu <sijian.gu@genewiz.com>
2017-02-06 17:10:57 +0800

    elastic-header.vue

--- src/component/elastic-header.vue
+++ src/component/elastic-header.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="draggable-header-view" 
 			@mousedown="startDrag" @touchstart="startDrag"
-			@mousemove="onDrag" @touchmove="onDrag"
-			@mouseup="stopDrag" @touchend="stopDrag"
+			@mousemove="onDrag" @touchmove="onDrag" 
+			@mouseup="stopDrag" @touchend="stopDrag" @mouseleave="stopDrag"
 			>
 		<svg class="bg" width="320" height="560">
 			<path :d="headerPath" fill="#3F51B5"></path>
